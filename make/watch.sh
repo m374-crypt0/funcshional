dbda043a8227243907d42f1e8d416e656853abcd
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:37:39 +0000

    test: enhanced watch behavior

--- make/watch.sh
+++ make/watch.sh
@@ -1,14 +1,14 @@
-function run_tests(){
+function run_tests() {
   # shellcheck source=/dev/null
   . "${ROOT_DIR}"make/test.sh
 }
 
 run_tests
 
-inotifywait -mqr \
-  --event modify \
-  "${ROOT_DIR}"test \
-  "${ROOT_DIR}"src \
-| while read -r; do
-  clear && run_tests
+while true; do
+  inotifywait -mqr \
+    --event modify \
+    "${ROOT_DIR}"test \
+    "${ROOT_DIR}"src |
+    run_tests
 done

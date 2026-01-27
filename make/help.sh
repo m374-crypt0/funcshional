47574e8dad8cf39218bf7e531380611e45fdcfb4
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 15:43:21 +0000

    docs: updated help target

--- make/help.sh
+++ make/help.sh
@@ -5,6 +5,7 @@ make <TARGET> where <TARGET> in:
 - help: print this message
 - init: initialize the project, especially dependencies
 - test: run all tests
+- watch: run all tests continuously, each time a file changed
 
 Do not forget to read the doc (README.md) at the root of this repository.
 EOF
